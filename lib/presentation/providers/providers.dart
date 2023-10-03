2fdd087a6566832ea452603e603d71b3039f2c19
Jonas Morales <jonasjosuemo2021@outlook.com>
2023-10-03 13:50:35 -0600

    Single Movie Provider

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -2,3 +2,4 @@ export 'movies/movie_repository_provider.dart';
 export 'movies/movies_providers.dart';
 export 'movies/movie_sliceshow_provider.dart';
 export 'movies/initial_loader_provider.dart';
+export 'movies/single_movie_details_provider.dart';
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,21 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movies_app/domian/entities/movie.dart';
+import 'package:movies_app/presentation/providers/providers.dart';
 
-class MovieScreen extends StatelessWidget {
-  
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie_screen';
 
   final String movieId;
 
-  const MovieScreen({
-    super.key, 
-    required this.movieId
-  });
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(sigleMovieDetailsProvider.notifier).loadMovie(widget.movieId);
+  }
 
   @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(sigleMovieDetailsProvider)[widget.movieId];
+
+    if (movie == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+        ),
+      );
+    }
     return Scaffold(
-      appBar: AppBar( title: Text(movieId)),
-      
+      appBar: AppBar(title: Text(widget.movieId)),
     );
   }
 }
